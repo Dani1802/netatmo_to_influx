1f9fbec6affff42d35ee3ec12d4073cfe1de6afa
Dani1802 <105320792+Dani1802@users.noreply.github.com>
2022-05-27 22:56:51 +0200

    Add files via upload

---
+++ netatmo_to_influx.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+### Variables
+USERNAME=
+PASSWORD=
+CLIENT_ID=
+CLIENT_SECRET=
+INFLUX_HOST=
+INFLUX_PORT=
+
+### Functions
+source functions
+
+### Main
+
+JSON=`getData`
+MODULES=`echo $JSON | jq '.body.devices[].modules[]'`
+
+#[TypeOfModule] [ModuleName] [DatabaseName]
+Base "Wohnzimmer" "Wohnzimmer"
+Base "Büro" "Büro"
+Indoor "Benjamin" "Benjamin"
+Indoor "Clara" "Clara"
+Indoor "Schlafzimmer" "Schlafzimmer"
+Indoor "Bad" "Bad"
+Indoor "Keller" "Keller"
+Outdoor "Garten" "Garten"
+Rain "Regen" "Regen"
+Wind "Wind" "Wind"
